1cd95b81f4688f4bd3cafb3ba26999c3f03a77a7
bryksindmitry <bryksindmitry@gmail.com>
2018-04-13 15:09:40 +0300

    test

--- src/components/Entrance/index.js
+++ src/components/Entrance/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Entrance(){
     return(
         <div className="entrance">
-            <a href="#" className="sign-in">Sign In</a>
+            <a href="#" className="sign-in">Sign In !</a>
             <a href="#" className="sign-up">Sign-Up</a>
         </div>
     )
