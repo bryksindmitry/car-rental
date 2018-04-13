066c33544335db7cf282833e0a258520362be582
Dmitry Bryksin <bryksindmitry@gmail.com>
2018-04-14 01:28:14 +0300

    add detail of offer and add style for Price

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react'
 import Price from '../Price'
 
 export default class Offer extends Component{
+
+    state = {
+        open : false
+    }
+
     render(){   
+
+        let {open} = this.state;
+        let detailNode =  this.state.open ? <div className ="offer__detail">
+            <p>Вы решили, что вы будете работать над проблемой №53 из системы отслеживания ошибок, используемой вашей компанией. Разумеется, Git не привязан к какой-то определенной системе </p>
+        </div> : "";
+
+        let textButton = open ? " - Hide Details":"+ View Details"
+
         return(
                 <div className="offer-item">
                     <div className="item-wrapper">
@@ -15,11 +28,20 @@ export default class Offer extends Component{
                             Nam nec tellus a odio tincidunt auctor a ornare odio
                         </div>
                     </div>
-                    <button className="offer__view-more">
-                        + View Details
+                    <button className="offer__view-more" onClick={ this.handleToggle }>
+                        { textButton }  
                     </button>
+                    { detailNode }
                     </div>
                 </div>
             )
+    }
+
+    handleToggle = ()=>{
+        this.setState(()=>{
+            return {
+                open : !this.state.open
+            }
+        })
     }
 }
