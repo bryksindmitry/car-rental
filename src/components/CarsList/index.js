d35195b1e2751458bc2a838ec4b02cef3d125b12
Dmitrii <bryksindmitry@gmail.com>
2018-04-23 23:20:18 +0300

    add CarList

---
+++ src/components/CarsList/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CarsList extends Component{
+    static propTypes = {
+
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
