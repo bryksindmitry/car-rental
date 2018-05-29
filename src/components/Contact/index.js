9741886df32b86a73429bd95a009f2f7e4fb1592
bryksindmitry <bryksindmitry@gmail.com>
2018-05-29 17:45:39 +0300

    add Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,7 @@ import  React, { Component } from 'react';
 
 import InputString from "../InputString";
 import TextArea from "../TextArea";
-
+import InputSubmit from "../InputSubmit";
 export default class Contact extends Component{
     render(){
         return(
@@ -10,17 +10,20 @@ export default class Contact extends Component{
                 <div className="contact__description">
                     Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.
                 </div>
-                <div className="contanct__string">
+                <div className="contact__string">
                     <InputString type={"text"} labelText={"Name"} />
                 </div>
-                <div className="contanct__string">
+                <div className="contact__string">
                     <InputString type={"email"} labelText={"E-mail"} />
                 </div>
-                <div className="contanct__string">
+                <div className="contact__phone">
                     <InputString type={"phone"} labelText={"Phone"}/>
                 </div>
-                <div>
+                <div className="contact__string">
                     <TextArea labelText={"Question / Message:"}/>
+                </div>
+                <div className="contact__button">
+                    <InputSubmit value="Send"/>
                 </div>
             </div>
         )
--- src/components/InputSubmit/index.js
+++ src/components/InputSubmit/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import InputString from "../InputString/index";
 
 export default function InputSubmit(props){
     let {value} = props;
